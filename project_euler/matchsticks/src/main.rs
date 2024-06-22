58a5a4c6e41415a31c9f00931cec728ffaac66d8
Jacek Grzybowski <totospl99@gmail.com>
2024-06-23 01:47:05 +0200

    changed token system

--- project_euler/matchsticks/src/main.rs
+++ project_euler/matchsticks/src/main.rs
@@ -1,192 +1,114 @@
 use indicatif::ProgressIterator;
 
-mod token;
-use token::Token;
+#[derive(Debug, Clone, Copy)]
+enum Operation {
+    Add,
+    Multiply,
+}
 
-#[derive(Debug, Clone)]
+impl Operation {
+    fn stick_value() -> u32 {
+        2
+    }
+
+    fn display(&self) -> String {
+        match self {
+            Operation::Add => String::from(" + "),
+            Operation::Multiply => String::from(" * "),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 struct Number {
     value: u32,
-    tokens: Vec<Token>,
+    stick_value: u32,
 }
 
 impl Number {
     fn new(value: u32) -> Number {
-        Number {
-            value,
-            tokens: value
-                .to_string()
-                .chars()
-                .map(|c| Token::from_value(c.to_digit(10).unwrap()).unwrap())
-                .collect(),
+        let stick_value = value
+            .to_string()
+            .chars()
+            .map(|c| match c {
+                '0' => 6,
+                '1' => 2,
+                '2' => 5,
+                '3' => 5,
+                '4' => 4,
+                '5' => 5,
+                '6' => 6,
+                '7' => 3,
+                '8' => 7,
+                '9' => 6,
+                _ => panic!("unexpected char {c}"),
+            })
+            .sum();
+
+        Number { value, stick_value }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Token {
+    Operation(Operation),
+    Number(Number),
+}
+
+impl Token {
+    fn stick_value(&self) -> u32 {
+        match self {
+            Token::Number(n) => n.stick_value,
+            Token::Operation(_) => Operation::stick_value(),
         }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Expression {
+    tokens: Vec<Token>,
+}
+
+impl Expression {
+    fn new(tokens: Vec<Token>) -> Expression {
+        Expression { tokens }
     }
 
     fn stick_value(&self) -> u32 {
         self.tokens.iter().map(|t| t.stick_value()).sum()
     }
-}
-
-#[derive(Debug, Clone)]
-struct Addition {
-    left: Box<Expression>,
-    right: Box<Expression>,
-}
-
-impl Addition {
-    fn stick_value(&self) -> u32 {
-        self.left.stick_value() + self.right.stick_value() + Token::Add.stick_value()
-    }
-
-    fn value(&self) -> u32 {
-        self.left.value() + self.right.value()
-    }
 
     fn display(&self) -> String {
-        let mut output = self.left.display().to_owned();
-        output.push_str(" + ");
-        output.push_str(&self.right.display());
-        output
+        self.tokens
+            .iter()
+            .map(|t| match t {
+                Token::Operation(o) => o.display(),
+                Token::Number(n) => n.value.to_string(),
+            })
+            .collect::<Vec<String>>()
+            .join("")
     }
 
-    fn optimize(&mut self, best_arr: &BestArr) {
-        let optimize_left = match *self.left {
-            Expression::Number(ref n) => {
-                Some(best_arr[n.value as usize].addition_expression.clone())
-            }
-            Expression::Addition(ref mut n) => {
-                n.optimize(best_arr);
-                None
-            }
-            Expression::Multiplication(ref mut n) => {
-                n.optimize(best_arr);
-                None
-            }
-        };
-
-        if let Some(left) = optimize_left {
-            self.left = Box::new(left);
-        }
-
-        let optimize_right = match *self.right {
-            Expression::Number(ref n) => {
-                Some(best_arr[n.value as usize].addition_expression.clone())
-            }
-            Expression::Addition(ref mut n) => {
-                n.optimize(best_arr);
-                None
-            }
-            Expression::Multiplication(ref mut n) => {
-                n.optimize(best_arr);
-                None
-            }
-        };
-
-        if let Some(right) = optimize_right {
-            self.right = Box::new(right);
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-struct Multiplication {
-    left: Box<Expression>,
-    right: Box<Expression>,
-}
-
-impl Multiplication {
-    fn stick_value(&self) -> u32 {
-        self.left.stick_value() + self.right.stick_value() + Token::Multiply.stick_value()
-    }
-
-    fn value(&self) -> u32 {
-        self.left.value() * self.right.value()
-    }
-
-    fn display(&self) -> String {
-        let mut output = self.left.display().to_owned();
-        output.push_str(" * ");
-        output.push_str(&self.right.display());
-        output
-    }
-
-    fn optimize(&mut self, best_arr: &BestArr) {
-        let optimize_left = match *self.left {
-            Expression::Number(ref n) => {
-                Some(best_arr[n.value as usize].multiplication_expression.clone())
-            }
-            Expression::Addition(ref mut n) => {
-                n.optimize(best_arr);
-                None
-            }
-            Expression::Multiplication(ref mut n) => {
-                n.optimize(best_arr);
-                None
-            }
-        };
-
-        if let Some(left) = optimize_left {
-            self.left = Box::new(left);
-        }
-
-        let optimize_right = match *self.right {
-            Expression::Number(ref n) => {
-                Some(best_arr[n.value as usize].multiplication_expression.clone())
-            }
-            Expression::Addition(ref mut n) => {
-                n.optimize(best_arr);
-                None
-            }
-            Expression::Multiplication(ref mut n) => {
-                n.optimize(best_arr);
-                None
-            }
-        };
-
-        if let Some(right) = optimize_right {
-            self.right = Box::new(right);
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-enum Expression {
-    Number(Number),
-    Addition(Addition),
-    Multiplication(Multiplication),
-}
-
-impl Expression {
-    fn stick_value(&self) -> u32 {
-        match &self {
-            Expression::Number(n) => n.stick_value(),
-            Expression::Addition(n) => n.stick_value(),
-            Expression::Multiplication(n) => n.stick_value(),
+    fn is_multiplication_replaceable(&self) -> bool {
+        if self
+            .tokens
+            .iter()
+            .find(|t| match t {
+                Token::Number(_) => false,
+                Token::Operation(o) => match o {
+                    Operation::Add => true,
+                    Operation::Multiply => false,
+                },
+            })
+            .is_some()
+        {
+            false
+        } else {
+            true
         }
     }
 
-    fn value(&self) -> u32 {
-        match &self {
-            Expression::Number(n) => n.value,
-            Expression::Addition(n) => n.value(),
-            Expression::Multiplication(n) => n.value(),
-        }
-    }
-
-    fn display(&self) -> String {
-        match &self {
-            Expression::Number(n) => n.value.to_string(),
-            Expression::Addition(n) => n.display(),
-            Expression::Multiplication(n) => n.display(),
-        }
-    }
-
-    fn optimize(&mut self, best_arr: &BestArr) {
-        match self {
-            Expression::Number(_) => {}
-            Expression::Addition(ref mut n) => n.optimize(best_arr),
-            Expression::Multiplication(ref mut n) => n.optimize(best_arr),
-        };
-    }
+    fn optimize(&mut self, bestArr: &BestArr) {}
 }
 
 /// returns vec with possible positive multiplications allowing to obtain number (ignoring 1 * n)
@@ -212,29 +134,30 @@ fn expand_number(n: u32) -> Vec<Expression> {
 
     let factors = binary_factorization(n);
 
-    for factor in factors {
-        res.push(Expression::Multiplication(Multiplication {
-            left: Box::new(Expression::Number(Number::new(factor.0))),
-            right: Box::new(Expression::Number(Number::new(factor.1))),
-        }))
+    for (a, b) in factors {
+        res.push(Expression::new(vec![
+            Token::Number(Number::new(a)),
+            Token::Operation(Operation::Multiply),
+            Token::Number(Number::new(b)),
+        ]));
     }
 
     for c in 1..n {
         let multiplication = n - c;
         let factors = binary_factorization(multiplication);
 
-        for factor in factors {
-            res.push(Expression::Addition(Addition {
-                left: Box::new(Expression::Number(Number::new(c))),
-                right: Box::new(Expression::Multiplication(Multiplication {
-                    left: Box::new(Expression::Number(Number::new(factor.0))),
-                    right: Box::new(Expression::Number(Number::new(factor.1))),
-                })),
-            }))
+        for (a, b) in factors {
+            res.push(Expression::new(vec![
+                Token::Number(Number::new(a)),
+                Token::Operation(Operation::Multiply),
+                Token::Number(Number::new(b)),
+                Token::Operation(Operation::Add),
+                Token::Number(Number::new(c)),
+            ]));
         }
     }
 
-    res.push(Expression::Number(Number::new(n)));
+    res.push(Expression::new(vec![Token::Number(Number::new(n))]));
 
     res
 }
@@ -249,19 +172,21 @@ struct BestNumber {
 fn main() {
     let mut best_arr: BestArr = vec![
         BestNumber {
-            addition_expression: Expression::Number(Number::new(0)),
-            multiplication_expression: Expression::Number(Number::new(0)),
+            addition_expression: Expression::new(vec![Token::Number(Number::new(0))]),
+            multiplication_expression: Expression::new(vec![Token::Number(Number::new(0))]),
         },
         BestNumber {
-            addition_expression: Expression::Number(Number::new(1)),
-            multiplication_expression: Expression::Number(Number::new(1)),
+            addition_expression: Expression::new(vec![Token::Number(Number::new(1))]),
+            multiplication_expression: Expression::new(vec![Token::Number(Number::new(1))]),
         },
     ];
 
-    let max_number = 100;
+    let max_number = 28;
 
-    for n in (2..max_number + 1).progress() {
+    // for n in (2..max_number + 1).progress() {
+    for n in 2..max_number + 1 {
         let mut combinations = expand_number(n);
+
         combinations.iter_mut().for_each(|c| c.optimize(&best_arr));
 
         let addition_expression = combinations
@@ -269,27 +194,22 @@ fn main() {
             .min_by(|x, y| x.stick_value().cmp(&y.stick_value()))
             .unwrap();
 
-        let multiplication_expression = combinations
-            .iter()
-            .filter(|e| match e {
-                Expression::Number(_) => true,
-                Expression::Multiplication(_) => true,
-                Expression::Addition(_) => false,
-            })
-            .min_by(|x, y| x.stick_value().cmp(&y.stick_value()))
-            .unwrap();
-
-        best_arr.push(BestNumber {
-            addition_expression: addition_expression.clone(),
-            multiplication_expression: multiplication_expression.clone(),
-        });
-
         println!(
             "{:?} = {:?}\t\t{:?}",
             n,
             addition_expression.display(),
             addition_expression.stick_value()
         );
+
+        best_arr.push(BestNumber {
+            addition_expression: addition_expression.to_owned(),
+            multiplication_expression: combinations
+                .iter()
+                .filter(|e| e.is_multiplication_replaceable())
+                .min_by(|x, y| x.stick_value().cmp(&y.stick_value()))
+                .unwrap()
+                .to_owned(),
+        });
     }
 
     let sum_t = best_arr
