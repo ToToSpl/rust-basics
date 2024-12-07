366d9ad8974a0ef3e0517fa80a811587387e331c
Jacek Grzybowski <totospl99@gmail.com>
2024-12-07 15:25:57 +0100

    day7 recur

--- advent_2024/day7/src/main.rs
+++ advent_2024/day7/src/main.rs
@@ -1,5 +1,5 @@
 use rayon::prelude::*;
-use std::fs;
+use std::{fs, time};
 
 const INPUT: &str = "input.txt";
 
@@ -19,7 +19,6 @@ impl Calibration {
         }
     }
 
-    // so fast that didnt even bother to optimize
     fn can_calibrate_brute(&self) -> Option<u64> {
         // 0 means addition, 1 means multiplication
         let mask_len = self.inputs.len() - 1;
@@ -47,6 +46,22 @@ impl Calibration {
         }
 
         None
+    }
+
+    fn can_calibrate_rec(&self) -> Option<u64> {
+        if _rec_two(self.value, self.inputs.len() - 1, &self.inputs) {
+            Some(self.value)
+        } else {
+            None
+        }
+    }
+
+    fn can_calibrate_three_rec(&self) -> Option<u64> {
+        if _rec_three(self.value, self.inputs.len() - 1, &self.inputs) {
+            Some(self.value)
+        } else {
+            None
+        }
     }
 
     fn can_calibrate_three_brute(&self) -> Option<u64> {
@@ -85,6 +100,65 @@ impl Calibration {
     }
 }
 
+fn _rec_two(value: u64, index: usize, inputs: &[u64]) -> bool {
+    let v = inputs[index];
+    if index == 0 {
+        value == v
+    } else if value > v && _rec_two(value - v, index - 1, inputs) {
+        true
+    } else if value % v == 0 && _rec_two(value / v, index - 1, inputs) {
+        true
+    } else {
+        false
+    }
+}
+
+fn _rec_three(value: u64, index: usize, inputs: &[u64]) -> bool {
+    let v = inputs[index];
+    if index == 0 {
+        value == v
+    } else if value > v && _rec_three(value - v, index - 1, inputs) {
+        true
+    } else if value % v == 0 && _rec_three(value / v, index - 1, inputs) {
+        true
+    } else {
+        let v_digits = v.checked_ilog10().unwrap_or(0) + 1;
+        let ten_mul = 10u64.pow(v_digits);
+
+        if value % ten_mul == v && _rec_three(value / ten_mul, index - 1, inputs) {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+fn task2_fast() {
+    let contents = fs::read_to_string(INPUT).unwrap();
+
+    let count = contents
+        .lines()
+        .map(Calibration::new)
+        .collect::<Vec<Calibration>>()
+        .par_iter()
+        .filter_map(|c| c.can_calibrate_three_rec())
+        .sum::<u64>();
+
+    println!("task2:\t{count}");
+}
+
+fn task1_fast() {
+    let contents = fs::read_to_string(INPUT).unwrap();
+
+    let count = contents
+        .lines()
+        .map(Calibration::new)
+        .filter_map(|c| c.can_calibrate_rec())
+        .sum::<u64>();
+
+    println!("task1:\t{count}");
+}
+
 fn task2() {
     let contents = fs::read_to_string(INPUT).unwrap();
 
@@ -112,6 +186,15 @@ fn task1() {
 }
 
 fn main() {
+    println!("slow");
+    let start = time::SystemTime::now();
     task1();
     task2();
+    println!("{:?}", start.elapsed().unwrap());
+
+    println!("fast");
+    let start = time::SystemTime::now();
+    task1_fast();
+    task2_fast();
+    println!("{:?}", start.elapsed().unwrap());
 }
