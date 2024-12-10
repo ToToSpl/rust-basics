a1a9ee1a779eea5079b0ea9924c99c5d1f9d467e
Jacek Grzybowski <totospl99@gmail.com>
2024-12-10 23:05:50 +0100

    day10 simplification

--- advent_2024/day10/src/main.rs
+++ advent_2024/day10/src/main.rs
@@ -99,20 +99,12 @@ fn task2() {
         .filter_map(|p| p)
         .collect::<Vec<(usize, usize)>>();
 
-    let tracks: Vec<((usize, usize), Vec<(usize, usize)>)> = zeros_pos
+    let scores = zeros_pos
         .iter()
-        .filter_map(|&(y, x)| {
-            let mut marked_map = vec![vec![false; map[0].len()]; map.len()];
-            marked_map[y][x] = true;
-
-            fn recur(
-                map: &Vec<Vec<i8>>,
-                marked_map: &mut Vec<Vec<bool>>,
-                cy: i64,
-                cx: i64,
-            ) -> Vec<(usize, usize)> {
+        .map(|(y, x)| {
+            fn recur(map: &Vec<Vec<i8>>, cy: i64, cx: i64) -> u64 {
                 if map[cy as usize][cx as usize] == 9 {
-                    return vec![(cy as usize, cx as usize)];
+                    return 1;
                 }
 
                 let cands = [(0, 1), (0, -1), (1, 0), (-1, 0)]
@@ -123,7 +115,6 @@ fn task2() {
                             && cx + dx >= 0
                             && cy + dy < map.len() as i64
                             && cx + dx < map[0].len() as i64
-                            && marked_map[(cy + dy) as usize][(cx + dx) as usize] == false
                             && map[(cy + dy) as usize][(cx + dx) as usize]
                                 - map[cy as usize][cx as usize]
                                 == 1
@@ -131,74 +122,19 @@ fn task2() {
                     .map(|(dy, dx)| ((cy + dy) as usize, (cx + dx) as usize))
                     .collect::<Vec<(usize, usize)>>();
 
-                let mut tracks = vec![];
+                let mut sum = 0;
 
                 for (y, x) in cands {
-                    marked_map[y][x] = true;
-                    let mut new_trail = recur(map, marked_map, y as i64, x as i64);
-                    if new_trail.len() > 0 {
-                        tracks.append(&mut new_trail);
-                    } else {
-                        marked_map[y][x] = false;
+                    let new_trail = recur(map, y as i64, x as i64);
+                    if new_trail > 0 {
+                        sum += new_trail;
                     }
                 }
 
-                tracks
+                sum
             }
 
-            let tracks = recur(&map, &mut marked_map, y as i64, x as i64);
-
-            if tracks.len() > 0 {
-                Some(((y, x), tracks))
-            } else {
-                None
-            }
-        })
-        .collect();
-
-    let scores = tracks
-        .iter()
-        .map(|((y, x), nines)| {
-            let path_scores = nines
-                .iter()
-                .map(|&(ny, nx)| {
-                    fn recur(map: &Vec<Vec<i8>>, cy: i64, cx: i64, ny: i64, nx: i64) -> u64 {
-                        if map[cy as usize][cx as usize] == 9 && cy == ny && cx == nx {
-                            return 1;
-                        }
-
-                        let cands = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-                            .iter()
-                            .filter(|(dy, dx)| {
-                                cy + dy >= 0
-                                    && cy + dy >= 0
-                                    && cx + dx >= 0
-                                    && cy + dy < map.len() as i64
-                                    && cx + dx < map[0].len() as i64
-                                    && map[(cy + dy) as usize][(cx + dx) as usize]
-                                        - map[cy as usize][cx as usize]
-                                        == 1
-                            })
-                            .map(|(dy, dx)| ((cy + dy) as usize, (cx + dx) as usize))
-                            .collect::<Vec<(usize, usize)>>();
-
-                        let mut sum = 0;
-
-                        for (y, x) in cands {
-                            let new_trail = recur(map, y as i64, x as i64, ny, nx);
-                            if new_trail > 0 {
-                                sum += new_trail;
-                            }
-                        }
-
-                        sum
-                    }
-
-                    recur(&map, *y as i64, *x as i64, ny as i64, nx as i64)
-                })
-                .collect::<Vec<u64>>();
-
-            path_scores.iter().sum()
+            recur(&map, *y as i64, *x as i64)
         })
         .collect::<Vec<u64>>();
 
